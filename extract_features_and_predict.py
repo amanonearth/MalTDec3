dddc9dae947e4dd6d624b0caab75c316701979f6
Mr00Magician <meanasnadeem@gmail.com>
2022-08-23 20:37:24 +0530

    updated extract_features_and_predict.py

--- extract_features_and_predict.py
+++ extract_features_and_predict.py
@@ -11,73 +11,74 @@ final_df = []
 next_row_index = 0
 
 def read():
+	# This function is not complete yet...
 	global next_row_index
 	dataframe = []
 	row = ' '
 	while ex.new_rows_count > 0:
-			row = ex.packetRow[next_row_index]
-			protocol_type = service = ''
-			failed_login = root_shell = su_attempted = file_creation = 0
-			file_access = outbound_conn = log_accessed = 0
-			if '' in row[3]:
-				protocol_type = 'TCP'
-			elif '' in row[3]:
-				protocol_type = 'SSH'
-			elif '' in row[3]:
-				protocol_type = 'FTP'
-			if '' in row[3]:
-				service = 'VSFTPD'
-			elif '' in row[3]:
-				service = 'SSHv2'
-			elif '' in row[3]:
-				service = 'FTP'
+		row = ex.packetRow[next_row_index]
+		protocol_type = service = ''
+		failed_login = root_shell = su_attempted = file_creation = 0
+		file_access = outbound_conn = log_accessed = 0
+		if '' in row[3]:
+			protocol_type = 'TCP'
+		elif '' in row[3]:
+			protocol_type = 'SSH'
+		elif '' in row[3]:
+			protocol_type = 'FTP'
+		if '' in row[3]:
+			service = 'VSFTPD'
+		elif '' in row[3]:
+			service = 'SSHv2'
+		elif '' in row[3]:
+			service = 'FTP'
 
-			if protocol_type == 'TCP':
-				if 'Login incorrect' in row[3]:
-					failed_login = 1
-				if 'USER root' in row[3]:
-					root_shell = 1
-				if 'USER root' in row[3]:
-					su_attempted = 1
-				if 'MKD' in row[3]:
-					file_creation = 1
-				if 'LIST' in row[3] or 'CWD' in row[3]:
-					file_access = 1
-				if '/var/log' in row[3]:
-					log_accessed = 1
-			elif protocol_type == 'SSH':
-				if 'Login incorrect' in row[3]:
-					failed_login = 1
-				if 'USER root' in row[3]:
-					root_shell = 1
-				if 'USER root' in row[3]:
-					su_attempted = 1
-				if 'MKD' in row[3]:
-					file_creation = 1
-				if 'LIST' in row[3] or 'CWD' in row[3]:
-					file_access = 1
-				if '/var/log' in row[3]:
-					log_accessed = 1
-			elif protocol_type == 'FTP':
-				if 'Login incorrect' in row[3]:
-					failed_login = 1
-				if 'USER root' in row[3]:
-					root_shell = 1
-				if 'USER root' in row[3]:
-					su_attempted = 1
-				if 'MKD' in row[3]:
-					file_creation = 1
-				if 'LIST' in row[3] or 'CWD' in row[3]:
-					file_access = 1
-				if '/var/log' in row[3]:
-					log_accessed = 1
-			
-			dataframe.append([protocol_type, service, row[0], row[1],
-				failed_login, root_shell, su_attempted, file_creation, 
-				file_access, outbound_conn, log_accessed, row[2]])
+		if protocol_type == 'TCP':
+			if 'Login incorrect' in row[3]:
+				failed_login = 1
+			if 'USER root' in row[3]:
+				root_shell = 1
+			if 'USER root' in row[3]:
+				su_attempted = 1
+			if 'MKD' in row[3]:
+				file_creation = 1
+			if 'LIST' in row[3] or 'CWD' in row[3]:
+				file_access = 1
+			if '/var/log' in row[3]:
+				log_accessed = 1
+		elif protocol_type == 'SSH':
+			if 'Login incorrect' in row[3]:
+				failed_login = 1
+			if 'USER root' in row[3]:
+				root_shell = 1
+			if 'USER root' in row[3]:
+				su_attempted = 1
+			if 'MKD' in row[3]:
+				file_creation = 1
+			if 'LIST' in row[3] or 'CWD' in row[3]:
+				file_access = 1
+			if '/var/log' in row[3]:
+				log_accessed = 1
+		elif protocol_type == 'FTP':
+			if 'Login incorrect' in row[3]:
+				failed_login = 1
+			if 'USER root' in row[3]:
+				root_shell = 1
+			if 'USER root' in row[3]:
+				su_attempted = 1
+			if 'MKD' in row[3]:
+				file_creation = 1
+			if 'LIST' in row[3] or 'CWD' in row[3]:
+				file_access = 1
+			if '/var/log' in row[3]:
+				log_accessed = 1
+		
+		dataframe.append([protocol_type, service, row[0], row[1],
+			failed_login, root_shell, su_attempted, file_creation, 
+			file_access, outbound_conn, log_accessed, row[2]])
 
-			next_row_index += 1
-			ex.new_rows_count -= 1
+		next_row_index += 1
+		ex.new_rows_count -= 1
 
 	return pd.DataFrame(dataframe, columns = FEATURES)
 
@@ -85,11 +86,10 @@ def merge(df1, df2):
 	return pd.concat([df1, df2], axis = 1)
 
 def read_and_merge(data):
+	global final_df
 	if type(data) != pd.DataFrame:
 		return read()
-	return merge(data, read())
-
-# final_df = read_and_merge(final_df)
+	final_df = merge(data, read())
 
 def get_mal_IPs():
 	global final_df
@@ -109,5 +109,5 @@ def get_mal_IPs():
 	return Mal_df
 
 
-# Calling all_read_and_merge() will read extract features and merge them to 'final_df'
+# Calling read_and_merge() will read and extract features and merge them to 'final_df'
 # Calling get_mal_IPs will return a dataframe containing src and dest IP of Malicious data points in 'final_df'
