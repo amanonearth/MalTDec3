da9e252a2c7573b80207b78914c1476022727d3d
Aman Srivastava <88317324+amanonearth@users.noreply.github.com>
2022-08-24 13:18:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import multiprocessing
-from external import ftp_sniffer, ssh_sniffer, tcp_sniffer
+from external import ftp_sniffer, ssh_sniffer, tcp_sniffer, http_sniffer
 from layers import layer1_process, layer2_process, layer3_process
 
 
@@ -7,6 +7,7 @@ from layers import layer1_process, layer2_process, layer3_process
 sniffer_ftp = multiprocessing.Process(target=ftp_sniffer)
 sniffer_ssh = multiprocessing.Process(target=ssh_sniffer)
 sniffer_tcp = multiprocessing.Process(target=tcp_sniffer)
+sniffer_http = multiprocessing.Process(target=http_sniffer)
 layer1 = multiprocessing.Process(target=layer1_process)
 layer2 = multiprocessing.Process(target=layer2_process)
 layer3 = multiprocessing.Process(target=layer3_process)
@@ -20,6 +21,7 @@ def layers_inspection():
         sniffer_tcp.join()
         sniffer_ftp.join()
         sniffer_ssh.join()
+        sniffer_http.join()
         layer1.join()
         layer2.join()
         layer3.join()
@@ -28,12 +30,14 @@ def layers_inspection():
         sniffer_tcp.start()
         sniffer_ftp.start()
         sniffer_ssh.start()
+        sniffer_http.start()
         layer1.start()
         layer2.start()
         layer3.start()
         sniffer_tcp.join()
         sniffer_ftp.join()
         sniffer_ssh.join()
+        sniffer_http.join()
         layer1.join()
         layer2.join()
         layer3.join()
@@ -45,16 +49,19 @@ def layers_inspection_1():
         sniffer_tcp.join()
         sniffer_ftp.join()
         sniffer_ssh.join()
+        sniffer_http.join()
  
     elif not started:
         started = True
         sniffer_tcp.start()
         sniffer_ftp.start()
         sniffer_ssh.start()
+        sniffer_http.start()
         sniffer_tcp.join()
         sniffer_ftp.join()
         sniffer_ssh.join()
+        sniffer_http.join()
 
 
 if __name__ == '__main__':
-    layers_inspection_1()
+    layers_inspection()
